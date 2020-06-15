dbffb6dd5dba651b315c934f089d614772a089c3
kichinosukey <kichinosuke.fukuhara@gmail.com>
2020-06-15 21:25:08 +0900

    [update] done tutolials

--- app_01.py
+++ app_01.py
@@ -1,3 +1,5 @@
+import time
+
 import streamlit as st
 
 import numpy as np
@@ -5,6 +7,20 @@ import pandas as pd
 from sklearn.datasets import load_boston
 
 if __name__ == '__main__':
+
+    'Starting a long computation...'
+
+    # Add a placeholder
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        # Update the progress bar with each iteration.
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
 
     data = load_boston()
     X = data.data
